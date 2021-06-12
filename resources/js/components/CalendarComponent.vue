24cf491e5f1508e01542f978fdbcffb780b47511
Vivek Kumar <kumar97vivek@gmail.com>
2021-06-12 21:58:08 +0530

    Add calendar component

---
+++ resources/js/components/CalendarComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+		<full-calendar :events="scheduledEvents" locale="en"></full-calendar>
+    </div>
+</template>
+
+<script>
+	var demoEvents = [
+		{
+			title : 'Sunny Out of Office',
+			start : '2021-06-25',
+			end : '2021-06-27'
+		},
+		{
+			title : 'Munny Out of Office',
+			start : '2021-06-26',
+			end : '2021-06-28'
+		}
+	];
+	export default {
+        data () {
+			return {
+			scheduledEvents: []
+			}
+		},
+		created() {
+			this.scheduledEvents = demoEvents;
+		},
+    }
+</script>
+
+<style scoped>
+h3 {
+    color: brown;
+}
+</style>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -28,7 +28,7 @@
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav mr-auto">
 			<li class="nav-item active">
-				<a class="nav-link" href="#">Home</a>
+				<a class="nav-link" href="{{ route('home') }}">Home</a>
 			</li>
 			<li class="nav-item active">
 				<a class="nav-link" href="{{ route('events.show') }}">Events</a>
